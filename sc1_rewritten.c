ad2a394573a87c41d82a5d60b85e616bc6e42da4
aaronsens <36092424+aaronsens@users.noreply.github.com>
2019-04-17 20:22:43 -0400

    fixed the type errors

--- sc1_rewritten.c
+++ sc1_rewritten.c
@@ -1,56 +1,62 @@
 #include <stdio.h>
-int main()
-{
 
-
-    char local_19[4] = {0x4, 0x3, 0x2, 0x1 }; //67305985
-    int local_15 = 0;
-    char local_1b[2] = {0x98, 0x44} //38980
-
-    rotate(local_19, 4);
-    xor(&local_19, 4, &local_1b, 2);
-    add(&local_19, 4, &local_1b, 2);
-    xor(&local_19, 4, &local_1b, 2);
-    rotate(&local_19, 4);
-    printf("Final: %s\n", local_19);
-}
-
-void rotate(int param_1, size_t param_2)
+void rotate(char* param_1, int param_2)
 {   int i = 0;
     //void *pvVar1; --> memcopy??? 
-    while(i < param_2) {
-       int *ptr[i] = param_1[param_2 + i -1]; 
+    char temp[param_2];
+    while(i <= param_2) {
+      temp[i] = param_1[param_2 + i -1]; 
        i +=1;
     }
 
     int j = 0;
-    while(j < param_2){
-        param_1[j] = *ptr[j];
+    while(j <= param_2){
+        param_1[j] = temp[j];
         j +=1;
     }
     return;
 }
 
 
-void xor(int param_1,uint param_2,int param_3,uint param_4)
+void xor(char* param_1,unsigned int param_2,char* param_3,unsigned int param_4)
 {
    int i = 0;
-   while(i<param_2) {
+   while(i<=param_2) {
 
-       param_1[i] = param_1[i]^param_3(i%param_4);
+       param_1[i] = param_1[i]^(param_3[i] +(i%param_4));
        i +=1;
    }
 
 }
 
 
-void add(int param_1,uint param_2,int param_3,uint param_4) 
+void add(char* param_1,unsigned int param_2, char* param_3, unsigned int param_4) 
 {
     int i = 0;
 
-    while(i <param_2) {
+    while(i <=param_2) {
        param_1[i] = param_1[i] + (i%param_4);
        i +=1;
     }
     return;
 }
+
+int main()
+{
+
+
+    char local_19[4] = {0x4, 0x3, 0x2, 0x1 }; //67305985
+    int local_15 = 0;
+    char local_1b[2] = {0x98, 0x44}; //38980
+    printf("Start: %s\n", local_19);
+    rotate(local_19, 4);
+    xor(&local_19, 4, &local_1b, 2);
+    add(&local_19, 4, &local_1b, 2);
+    xor(&local_19, 4, &local_1b, 2);
+    rotate(&local_19, 4);
+
+    printf("End: %s\n", local_19);
+}
+
+
+
